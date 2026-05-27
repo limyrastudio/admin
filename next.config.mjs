2380fdca73a265aba0d6231c600a3cbde1da60b2
damas2000 <hghannom@gmail.com>
2026-05-27 07:27:55 +0300

    feat: Cloudflare Pages setup, production API URL, UI improvements, bug fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
+};
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
